e3eebbd5c483387275d3fa65ca1cdc6bc1a95dd5
kylin1209 <144865190+kylin1209@users.noreply.github.com>
2025-10-18 11:28:33 -0400

    fixed items display

--- python_backend/app/__init__.py
+++ python_backend/app/__init__.py
@@ -75,7 +75,7 @@ def load_user(user_id):
 
 # Configure CORS
 CORS(app, 
-     resources={r"/api/*": {
+     resources={r"/*": {
          "origins": ["http://127.0.0.1:5500", "http://localhost:5500"],
          "allow_credentials": True,
          "expose_headers": ["Set-Cookie", "Authorization"],
